9a866bd67b796b09d388d559e99ce105130ac4c1
Ben Whitmore <32911992+benwh1@users.noreply.github.com>
2022-04-09 20:59:14 +0100

    impl Display for Direction

--- src/algorithm/direction.rs
+++ src/algorithm/direction.rs
@@ -1,3 +1,5 @@
+use std::fmt::Display;
+
 #[derive(Clone, Copy, Debug, PartialEq, Eq, PartialOrd, Ord, Hash)]
 pub enum Direction {
     Up,
@@ -14,5 +16,20 @@ impl Direction {
             Self::Down => Self::Up,
             Self::Right => Self::Left,
         }
+    }
+}
+
+impl Display for Direction {
+    fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
+        write!(
+            f,
+            "{}",
+            match self {
+                Self::Up => "U",
+                Self::Left => "L",
+                Self::Down => "D",
+                Self::Right => "R",
+            }
+        )
     }
 }
