7dd3c27ec6325440cae76f233f88ea0cb1f7a38d
Ben Whitmore <32911992+benwh1@users.noreply.github.com>
2022-04-09 20:17:02 +0100

    Algorithm struct

---
+++ src/algorithm/algorithm.rs
@@ -0,0 +1,19 @@
+use super::puzzle_move::Move;
+
+pub struct Algorithm {
+    moves: Vec<Move>,
+}
+
+impl Algorithm {
+    fn new(moves: Vec<Move>) -> Self {
+        Self { moves }
+    }
+
+    fn length(&self) -> u32 {
+        self.moves.iter().map(|m| m.amount).sum()
+    }
+
+    fn push(&mut self, m: Move) {
+        self.moves.push(m)
+    }
+}
