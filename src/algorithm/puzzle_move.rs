74ebdfee2db4ceba0ab8735acbd0bbc28b6938b9
Ben Whitmore <32911992+benwh1@users.noreply.github.com>
2022-04-10 19:55:07 +0100

    Add thiserror crate and add error handling to Move constructor

--- src/algorithm/puzzle_move.rs
+++ src/algorithm/puzzle_move.rs
@@ -1,4 +1,5 @@
 use super::{direction::Direction, display::puzzle_move::DisplayMove};
+use thiserror::Error;
 
 #[derive(Clone, Copy, Debug, PartialEq, Eq, PartialOrd, Ord, Hash)]
 pub struct Move {
@@ -6,7 +7,21 @@ pub struct Move {
     pub amount: u32,
 }
 
+#[derive(Clone, Debug, Error, PartialEq, Eq, PartialOrd, Ord, Hash)]
+pub enum MoveError {
+    #[error("InvalidAmount: `amount` ({0}) must be greater than 0")]
+    InvalidAmount(u32),
+}
+
 impl Move {
+    pub fn new(direction: Direction, amount: u32) -> Result<Self, MoveError> {
+        if amount > 0 {
+            Ok(Self { direction, amount })
+        } else {
+            Err(MoveError::InvalidAmount(amount))
+        }
+    }
+
     pub fn inverse(&self) -> Self {
         Self {
             direction: self.direction.inverse(),
