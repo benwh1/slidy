61e1258f676c7c911bdb4900bab76bfb8f3eab12
Ben Whitmore <32911992+benwh1@users.noreply.github.com>
2022-04-10 03:18:49 +0100

    Display structs for Algorithm

--- src/algorithm/algorithm.rs
+++ src/algorithm/algorithm.rs
@@ -1,7 +1,22 @@
-use super::puzzle_move::Move;
+use super::puzzle_move::{Move, MoveDisplay};
+use std::{fmt::Display, marker::PhantomData};
 
 pub struct Algorithm {
     moves: Vec<Move>,
+}
+
+pub struct DisplaySpaced;
+pub struct DisplayUnspaced;
+
+pub trait AlgorithmDisplay {}
+
+impl AlgorithmDisplay for DisplaySpaced {}
+impl AlgorithmDisplay for DisplayUnspaced {}
+
+pub struct DisplayAlgorithm<'a, T1: AlgorithmDisplay, T2: MoveDisplay> {
+    alg: &'a Algorithm,
+    phantom_t1: PhantomData<T1>,
+    phantom_t2: PhantomData<T2>,
 }
 
 impl Algorithm {
