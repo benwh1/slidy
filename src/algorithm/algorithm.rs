409ab58150e94868f224706ef31be174eafb6c1f
Ben Whitmore <32911992+benwh1@users.noreply.github.com>
2022-04-10 16:09:28 +0100

    Update DisplayAlgorithm and implement DisplaySpaced

--- src/algorithm/algorithm.rs
+++ src/algorithm/algorithm.rs
@@ -1,5 +1,5 @@
-use super::puzzle_move::{Move, MoveDisplay};
-use std::{fmt::Display, marker::PhantomData};
+use super::puzzle_move::{DisplayMove, Move, MoveDisplay};
+use std::marker::PhantomData;
 
 pub struct Algorithm {
     moves: Vec<Move>,
@@ -8,12 +8,11 @@ pub struct Algorithm {
 pub struct DisplaySpaced;
 pub struct DisplayUnspaced;
 
-pub trait AlgorithmDisplay {}
+pub trait AlgorithmDisplay {
+    fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result;
+}
 
-impl AlgorithmDisplay for DisplaySpaced {}
-impl AlgorithmDisplay for DisplayUnspaced {}
-
-pub struct DisplayAlgorithm<'a, T1: AlgorithmDisplay, T2: MoveDisplay> {
+pub struct DisplayAlgorithm<'a, T1, T2> {
     alg: &'a Algorithm,
     phantom_t1: PhantomData<T1>,
     phantom_t2: PhantomData<T2>,
@@ -30,5 +29,23 @@ impl Algorithm {
 
     fn push(&mut self, m: Move) {
         self.moves.push(m)
+    }
+}
+
+impl<'a, T> AlgorithmDisplay for DisplayAlgorithm<'a, DisplaySpaced, T>
+where
+    DisplayMove<'a, T>: MoveDisplay,
+{
+    fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
+        write!(
+            f,
+            "{}",
+            self.alg
+                .moves
+                .iter()
+                .map(|m| m.display::<T>().to_string())
+                .intersperse(" ".to_string())
+                .collect::<String>()
+        )
     }
 }
--- src/lib.rs
+++ src/lib.rs
@@ -1,4 +1,5 @@
 #![feature(adt_const_params)]
+#![feature(iter_intersperse)]
 
 pub mod algorithm;
 pub mod puzzle;
