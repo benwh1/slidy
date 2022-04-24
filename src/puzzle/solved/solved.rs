9588656446a7d43aa238445e156153b60e39bd98
Ben Whitmore <32911992+benwh1@users.noreply.github.com>
2022-04-24 22:52:10 +0100

    Columns solved state

--- src/puzzle/solved/solved.rs
+++ src/puzzle/solved/solved.rs
@@ -9,6 +9,7 @@ where
 }
 
 struct Rows;
+struct Columns;
 
 impl<Piece, Puzzle> SolvedState<Piece, Puzzle> for Rows
 where
@@ -21,6 +22,33 @@ where
         }
 
         (0..puzzle.num_pieces()).all(|i| puzzle.piece_at(i).into() == (i + 1) as u64)
+    }
+}
+
+impl<Piece, Puzzle> SolvedState<Piece, Puzzle> for Columns
+where
+    Piece: Into<u64>,
+    Puzzle: SlidingPuzzle<Piece>,
+{
+    fn is_solved(puzzle: &Puzzle) -> bool {
+        if puzzle.gap_position() != puzzle.num_pieces() {
+            return false;
+        }
+
+        let (w, h) = puzzle.size();
+        for y in 0..h {
+            for x in 0..w {
+                if (x, y) == (w - 1, h - 1) {
+                    continue;
+                }
+
+                if puzzle.piece_at_xy(x, y).into() != (1 + y + h * x) as u64 {
+                    return false;
+                }
+            }
+        }
+
+        true
     }
 }
 
@@ -55,6 +83,35 @@ mod tests {
         fn test_rows_4() {
             let p = Puzzle::from_str("1 2/3 4/7 8/5 6/9 10/11 12/13 14/15 16/17 18/19 0").unwrap();
             assert!(!p.is_solved::<Rows>());
+        }
+    }
+
+    mod columns {
+        use super::*;
+        use crate::puzzle::solved::solved::Columns;
+
+        #[test]
+        fn test_columns() {
+            let p = Puzzle::from_str("1 5 9 13/2 6 10 14/3 7 11 15/4 8 12 0").unwrap();
+            assert!(p.is_solved::<Columns>());
+        }
+
+        #[test]
+        fn test_columns_2() {
+            let p = Puzzle::from_str("1 5 9 13/6 2 10 14/3 7 11 15/8 4 12 0").unwrap();
+            assert!(!p.is_solved::<Columns>());
+        }
+
+        #[test]
+        fn test_columns_3() {
+            let p = Puzzle::from_str("1 11/2 12/3 13/4 14/5 15/6 16/7 17/8 18/9 19/10 0").unwrap();
+            assert!(p.is_solved::<Columns>());
+        }
+
+        #[test]
+        fn test_columns_4() {
+            let p = Puzzle::from_str("1 11/2 13/3 12/4 14/6 15/5 16/7 17/8 18/9 19/10 0").unwrap();
+            assert!(!p.is_solved::<Columns>());
         }
     }
 }
