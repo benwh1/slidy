32461271775fd3f66d5406b8775b66d0771574cb
Ben Whitmore <32911992+benwh1@users.noreply.github.com>
2022-04-26 16:57:45 +0100

    Monochrome color scheme

--- src/puzzle/color_scheme.rs
+++ src/puzzle/color_scheme.rs
@@ -3,3 +3,13 @@ use palette::rgb::Rgb;
 trait ColorScheme {
     fn color(&self, label: usize, num_labels: usize) -> Rgb;
 }
+
+struct Monochrome {
+    color: Rgb,
+}
+
+impl ColorScheme for Monochrome {
+    fn color(&self, _label: usize, _num_labels: usize) -> Rgb {
+        self.color
+    }
+}
