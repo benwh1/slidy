00327fdc204e2f2d6451161fe27a07cb35fc6fd7
Ben Whitmore <32911992+benwh1@users.noreply.github.com>
2022-04-27 23:13:10 +0100

    ColorList new function

--- src/puzzle/color_scheme.rs
+++ src/puzzle/color_scheme.rs
@@ -1,11 +1,28 @@
 use palette::rgb::Rgb;
+use thiserror::Error;
 
 trait ColorScheme {
     fn color(&self, label: usize, num_labels: usize) -> Rgb;
 }
 
+#[derive(Error, Debug)]
+enum ColorSchemeError {
+    #[error("EmptyColorList: color list must be non-empty")]
+    EmptyColorList,
+}
+
 struct ColorList {
     colors: Vec<Rgb>,
+}
+
+impl ColorList {
+    pub fn new(colors: Vec<Rgb>) -> Result<Self, ColorSchemeError> {
+        if colors.is_empty() {
+            Err(ColorSchemeError::EmptyColorList)
+        } else {
+            Ok(Self { colors })
+        }
+    }
 }
 
 impl ColorScheme for ColorList {
