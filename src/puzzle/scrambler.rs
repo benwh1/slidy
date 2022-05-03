19bb5104b03168dfecf87d273dc2922745605906
Ben Whitmore <32911992+benwh1@users.noreply.github.com>
2022-05-03 13:36:21 +0100

    RandomState scrambler

--- src/puzzle/scrambler.rs
+++ src/puzzle/scrambler.rs
@@ -1,4 +1,6 @@
 use super::sliding_puzzle::SlidingPuzzle;
+use crate::algorithm::{direction::Direction, puzzle_move::Move};
+use rand::Rng;
 
 pub trait Scrambler<P, Piece>
 where
@@ -6,4 +8,43 @@ where
     Piece: Into<u64>,
 {
     fn scramble(puzzle: &mut P);
+}
+
+struct RandomState;
+
+impl<P, Piece> Scrambler<P, Piece> for RandomState
+where
+    P: SlidingPuzzle<Piece>,
+    Piece: Into<u64>,
+{
+    fn scramble(puzzle: &mut P) {
+        puzzle.reset();
+
+        let mut rng = rand::thread_rng();
+
+        let n = puzzle.num_pieces();
+        let mut parity = false;
+        for i in 0..n - 2 {
+            // Pick random element to go in position i
+            let j = rng.gen_range(i..n);
+
+            // Swap and check if we need to toggle parity
+            if i != j {
+                puzzle.swap_pieces(i, j);
+                parity = !parity;
+            }
+        }
+
+        // Swap the last two pieces if necessary to make it solvable
+        if parity {
+            puzzle.swap_pieces(n - 2, n - 1);
+        }
+
+        // Move blank to a random position
+        let (w, h) = puzzle.size();
+        let (d, r) = (rng.gen_range(0..h), rng.gen_range(0..w));
+
+        puzzle.apply_move(Move::new(Direction::Down, d as u32));
+        puzzle.apply_move(Move::new(Direction::Right, r as u32));
+    }
 }
