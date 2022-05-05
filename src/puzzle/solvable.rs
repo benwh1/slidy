697651be3ce11e799c2541e5988d4c630004180c
Ben Whitmore <32911992+benwh1@users.noreply.github.com>
2022-05-05 12:27:41 +0100

    Implement Solvable for RowGrids

--- src/puzzle/solvable.rs
+++ src/puzzle/solvable.rs
@@ -14,6 +14,57 @@ where
     fn solvable(puzzle: &Puzzle) -> bool;
 }
 
+impl<Piece, Puzzle> Solvable<Piece, Puzzle> for RowGrids
+where
+    Piece: Into<u64>,
+    Puzzle: SlidingPuzzle<Piece>,
+{
+    fn solvable(puzzle: &Puzzle) -> bool {
+        // Closure to get the piece that would be in position (x, y), if we do L* U* to move the
+        // gap to the bottom right corner
+        let (w, h) = puzzle.size();
+        let (gx, gy) = puzzle.gap_position_xy();
+        let piece_at = |i| {
+            let (x, y) = (i % w, i / w);
+            if x == w - 1 && y >= gy {
+                if y == h - 1 {
+                    // Gap piece
+                    puzzle.piece_at_xy(gx, gy)
+                } else {
+                    puzzle.piece_at_xy(x, y + 1)
+                }
+            } else if y == gy && x >= gx {
+                puzzle.piece_at_xy(x + 1, y)
+            } else {
+                puzzle.piece_at_xy(x, y)
+            }
+        };
+
+        let n = puzzle.num_pieces();
+        let mut seen = vec![false; n];
+        let mut parity = false;
+        for i in 0..n {
+            if seen[i] {
+                continue;
+            }
+
+            let mut index = i;
+            let mut cycle_len = 0;
+            while !seen[index] {
+                seen[index] = true;
+                cycle_len += 1;
+                index = puzzle.solved_pos(piece_at(index));
+            }
+
+            if cycle_len % 2 == 0 {
+                parity = !parity;
+            }
+        }
+
+        !parity
+    }
+}
+
 macro_rules! always_solvable {
     () => {};
     ($t:ty) => {
