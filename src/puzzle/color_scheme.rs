925c46a0da6c3650f89a031e89a7a9e8bad23c22
Ben Whitmore <32911992+benwh1@users.noreply.github.com>
2022-07-31 00:31:36 +0100

    Add Rainbow color scheme

--- src/puzzle/color_scheme.rs
+++ src/puzzle/color_scheme.rs
@@ -1,4 +1,4 @@
-use palette::rgb::Rgb;
+use palette::{rgb::Rgb, Gradient, Hsl, IntoColor};
 use thiserror::Error;
 
 pub trait ColorScheme {
@@ -15,6 +15,8 @@ pub struct ColorList {
     colors: Vec<Rgb>,
 }
 
+pub struct Rainbow;
+
 impl ColorList {
     pub fn new(colors: Vec<Rgb>) -> Result<Self, ColorSchemeError> {
         if colors.is_empty() {
@@ -28,5 +30,17 @@ impl ColorList {
 impl ColorScheme for ColorList {
     fn color(&self, label: usize, _num_labels: usize) -> Rgb {
         self.colors[label % self.colors.len()]
+    }
+}
+
+impl ColorScheme for Rainbow {
+    fn color(&self, label: usize, num_labels: usize) -> Rgb {
+        let colors = [
+            Hsl::new(0.0, 1.0, 0.5),
+            Hsl::new(165.0, 1.0, 0.5),
+            Hsl::new(330.0, 1.0, 0.5),
+        ];
+        let g = Gradient::new(colors);
+        g.get(label as f32 / num_labels as f32).into_color()
     }
 }
