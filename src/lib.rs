c47f244e90215a957030bb9abcad17cccdaa31f2
Ben Whitmore <32911992+benwh1@users.noreply.github.com>
2022-08-16 15:16:47 +0100

    Deny a few more clippy lints

--- src/lib.rs
+++ src/lib.rs
@@ -5,9 +5,12 @@
 #![feature(iter_intersperse)]
 #![feature(let_else)]
 #![allow(clippy::module_inception)]
-#![warn(clippy::use_self)]
 #![warn(clippy::must_use_candidate)]
+#![deny(clippy::use_self)]
 #![deny(clippy::double_must_use)]
+#![deny(clippy::if_not_else)]
+#![deny(clippy::iter_not_returning_iterator)]
+#![deny(clippy::mod_module_files)]
 
 pub mod algorithm;
 pub mod puzzle;
