58cef07a6a93f6165ce92f8166b2518b9c1569f8
Ben Whitmore <32911992+benwh1@users.noreply.github.com>
2022-08-16 16:12:46 +0100

    Add `RectPartition` struct to represent a partition of a rectangle into smaller rectangles. Verifies that the partition is actually a partition in O(number of sub-rectangles).

--- src/puzzle/color_scheme/recursive.rs
+++ src/puzzle/color_scheme/recursive.rs
@@ -1,3 +1,5 @@
+use std::{collections::BTreeMap, ops::Range};
+
 #[derive(Clone, Debug, PartialEq, Eq, PartialOrd, Ord, Hash)]
 struct Rect {
     left: u32,
@@ -13,6 +15,103 @@ impl Rect {
             left: top_left.0,
             bottom: bottom_right.1,
             right: bottom_right.0,
+        }
+    }
+}
+
+#[derive(Clone, Debug, PartialEq, Eq, PartialOrd, Ord, Hash)]
+struct PiecewiseConstant {
+    data: BTreeMap<u32, u32>,
+}
+
+impl PiecewiseConstant {
+    fn new() -> Self {
+        let mut data = BTreeMap::new();
+        data.insert(0, 0);
+        Self { data }
+    }
+
+    fn value(&self, x: u32) -> u32 {
+        self.data.range(..=x).last().map(|(_, &v)| v).unwrap()
+    }
+
+    fn range_value(&self, range: Range<u32>) -> Option<u32> {
+        let v = self.value(range.start);
+        if self.data.range(range).map(|(_, &v)| v).all(|x| x == v) {
+            Some(v)
+        } else {
+            None
+        }
+    }
+
+    fn set_range_value(&mut self, range: Range<u32>, value: u32) {
+        let next_point = self.data.range(range.end..).next().map(|(&k, &v)| (k, v));
+
+        let keys = self
+            .data
+            .range(range.clone())
+            .map(|(&k, _)| k)
+            .collect::<Vec<_>>();
+
+        let prev_value = range.start.checked_sub(1).map(|a| self.value(a));
+        let end_value = self.value(range.end);
+
+        for k in keys {
+            self.data.remove(&k);
+        }
+        if let Some((k, v)) = next_point && v == end_value {
+            self.data.remove(&k);
+        }
+
+        match prev_value {
+            Some(v) if value != v => {
+                self.data.insert(range.start, value);
+            }
+            None => {
+                self.data.insert(range.start, value);
+            }
+            _ => {}
+        }
+
+        self.data.insert(range.end, end_value);
+    }
+}
+
+struct RectPartition {
+    rects: Vec<Rect>,
+}
+
+impl RectPartition {
+    fn new(mut rects: Vec<Rect>) -> Option<Self> {
+        rects.sort_by_key(|r| (r.top, r.left));
+
+        let left = rects.iter().map(|r| r.left).min().unwrap();
+        let right = rects.iter().map(|r| r.right).max().unwrap();
+
+        let mut height_map = PiecewiseConstant::new();
+        height_map.data.insert(0, 0);
+
+        for slice in rects.group_by(|a, b| a.top == b.top) {
+            let y = slice[0].top;
+            for rect in slice {
+                let height = height_map.range_value(rect.left..rect.right);
+                if let Some(height) = height {
+                    if height == rect.top {
+                        height_map.set_range_value(rect.left..rect.right, rect.bottom);
+                    } else {
+                        return None;
+                    }
+                }
+            }
+            if height_map.data.values().find(|&&a| a == y).is_some() {
+                return None;
+            }
+        }
+
+        if height_map.range_value(left..right).is_some() {
+            Some(Self { rects })
+        } else {
+            None
         }
     }
 }
