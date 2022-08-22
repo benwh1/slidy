e144db35bbf66820b4a4c941287d121bbeceda21
Ben Whitmore <32911992+benwh1@users.noreply.github.com>
2022-08-22 12:04:23 +0100

    Fix `Scaled` and add a test

--- src/puzzle/label/scaled.rs
+++ src/puzzle/label/scaled.rs
@@ -33,8 +33,8 @@ impl<'a, L: Label> Label for Scaled<'a, L> {
     fn position_label_unchecked(&self, width: usize, height: usize, x: usize, y: usize) -> usize {
         let width = width.div_ceil(self.horizontal as usize);
         let height = height.div_ceil(self.vertical as usize);
-        let x = x.div_ceil(self.horizontal as usize);
-        let y = y.div_ceil(self.vertical as usize);
+        let x = x.div_floor(self.horizontal as usize);
+        let y = y.div_floor(self.vertical as usize);
         self.label.position_label_unchecked(width, height, x, y)
     }
 
@@ -42,5 +42,32 @@ impl<'a, L: Label> Label for Scaled<'a, L> {
         let width = width.div_ceil(self.horizontal as usize);
         let height = height.div_ceil(self.vertical as usize);
         self.label.num_labels_unchecked(width, height)
+    }
+}
+
+#[cfg(test)]
+mod tests {
+    use crate::puzzle::label::label::RowGrids;
+
+    use super::*;
+
+    #[test]
+    fn test_scaled_row_grids() {
+        let label = Scaled::new(&RowGrids, 3, 2).unwrap();
+
+        let labels = (0..40)
+            .map(|i| label.position_label_unchecked(8, 5, i % 8, i / 8))
+            .collect::<Vec<_>>();
+        let num_labels = label.num_labels_unchecked(8, 5);
+
+        #[rustfmt::skip]
+        assert_eq!(labels, vec![
+            0, 0, 0, 1, 1, 1, 2, 2,
+            0, 0, 0, 1, 1, 1, 2, 2,
+            3, 3, 3, 4, 4, 4, 5, 5,
+            3, 3, 3, 4, 4, 4, 5, 5,
+            6, 6, 6, 7, 7, 7, 8, 8,
+        ]);
+        assert_eq!(num_labels, 9);
     }
 }
