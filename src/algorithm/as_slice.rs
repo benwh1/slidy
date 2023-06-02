c543a60a1371b73887396fa8d16ad46ba09a5147
Ben Whitmore <32911992+benwh1@users.noreply.github.com>
2023-06-02 13:32:36 +0100

    Move `Algorithm::as_slice` test to `as_slice` module, and add one for `AlgorithmSlice`

--- src/algorithm/as_slice.rs
+++ src/algorithm/as_slice.rs
@@ -24,3 +24,44 @@ impl<'a> AsAlgorithmSlice<'a> for AlgorithmSlice<'a> {
         *self
     }
 }
+
+#[cfg(test)]
+mod tests {
+    use std::str::FromStr;
+
+    use crate::algorithm::{
+        algorithm::Algorithm, as_slice::AsAlgorithmSlice, r#move::r#move::Move,
+        slice::AlgorithmSlice,
+    };
+
+    #[test]
+    fn test_algorithm() {
+        let a = Algorithm::from_str("U2LD3R").unwrap();
+        let b = a.as_slice();
+        assert_eq!(
+            b,
+            AlgorithmSlice {
+                first: None,
+                middle: &a.moves,
+                last: None
+            }
+        );
+    }
+
+    #[test]
+    fn test_algorithm_slice() -> Result<(), Box<dyn std::error::Error>> {
+        let a = AlgorithmSlice {
+            first: Some(Move::from_str("R3")?),
+            middle: &[Move::from_str("D")?, Move::from_str("L2")?],
+            last: Some(Move::from_str("U6")?),
+        };
+        let b = a.as_slice();
+
+        assert_eq!(a, b);
+
+        // Make sure we aren't somehow doing a deep copy
+        assert_eq!(a.middle.as_ptr(), b.middle.as_ptr());
+
+        Ok(())
+    }
+}
