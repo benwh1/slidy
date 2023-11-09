965aa8f24ee3cb2b187efa9e1551a599b9228869
Ben Whitmore <32911992+benwh1@users.noreply.github.com>
2023-11-09 21:13:46 +0000

    Implement manhattan distance heuristic for `Rows` solved state

--- src/solver/heuristic.rs
+++ src/solver/heuristic.rs
@@ -4,7 +4,9 @@ use itertools::Itertools;
 use num_traits::{AsPrimitive, PrimInt, Unsigned, Zero};
 
 use crate::puzzle::{
-    label::labels::RowGrids, sliding_puzzle::SlidingPuzzle, solved_state::SolvedState,
+    label::labels::{RowGrids, Rows},
+    sliding_puzzle::SlidingPuzzle,
+    solved_state::SolvedState,
 };
 
 #[cfg(feature = "serde")]
@@ -42,5 +44,40 @@ where
             })
             .sum::<usize>()
             .as_()
+    }
+}
+
+impl<T: PrimInt + Unsigned + 'static> Heuristic<Rows, T> for ManhattanDistance
+where
+    usize: AsPrimitive<T>,
+{
+    fn bound<P: SlidingPuzzle>(&self, puzzle: &P) -> T {
+        let (w, h) = puzzle.size().into();
+        let md = (0..w)
+            .cartesian_product(0..h)
+            .map(|(x, y)| {
+                let piece = puzzle.piece_at_xy((x, y));
+                let (_, b) = puzzle.solved_pos_xy(piece);
+
+                if piece == P::Piece::zero() {
+                    0
+                } else {
+                    y.abs_diff(b)
+                }
+            })
+            .sum::<usize>();
+
+        // Make sure the parity is correct (some positions will give an even bound for a position
+        // that takes an odd number of moves, etc.)
+        let (gx, gy) = puzzle.gap_position_xy();
+        let (sx, sy) = puzzle.solved_pos_xy(P::Piece::zero());
+
+        let adjusted_md = if md % 2 != (gx.abs_diff(sx) + gy.abs_diff(sy)) % 2 {
+            md + 1
+        } else {
+            md
+        };
+
+        adjusted_md.as_()
     }
 }
