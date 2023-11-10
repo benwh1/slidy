995510be48b08d41259299693f0bb4097d1e8214
Ben Whitmore <32911992+benwh1@users.noreply.github.com>
2023-11-10 00:07:14 +0000

    Replace implementations of `Heuristic` on `ManhattanDistance` with a macro

--- src/solver/heuristic.rs
+++ src/solver/heuristic.rs
@@ -24,60 +24,52 @@ pub trait Heuristic<S: SolvedState, T: PrimInt + Unsigned> {
 #[cfg_attr(feature = "serde", derive(Serialize, Deserialize))]
 pub struct ManhattanDistance;
 
-impl<T: PrimInt + Unsigned + 'static> Heuristic<RowGrids, T> for ManhattanDistance
-where
-    usize: AsPrimitive<T>,
-{
-    fn bound<P: SlidingPuzzle>(&self, puzzle: &P) -> T {
-        let (w, h) = puzzle.size().into();
-        (0..w)
-            .cartesian_product(0..h)
-            .map(|(x, y)| {
-                let piece = puzzle.piece_at_xy((x, y));
-                let (a, b) = puzzle.solved_pos_xy(piece);
+macro_rules! impl_manhattan {
+    ($label:ty, $dist:expr, $parity_fix:literal $(,)?) => {
+        impl<T: PrimInt + Unsigned + 'static> Heuristic<$label, T> for ManhattanDistance
+        where
+            usize: AsPrimitive<T>,
+        {
+            fn bound<P: SlidingPuzzle>(&self, puzzle: &P) -> T {
+                let dist: fn((usize, usize), (usize, usize)) -> usize = $dist;
 
-                if piece == P::Piece::zero() {
-                    0
+                let (w, h) = puzzle.size().into();
+                let md = (0..w)
+                    .cartesian_product(0..h)
+                    .map(|pos| {
+                        let piece = puzzle.piece_at_xy(pos);
+                        let solved_pos = puzzle.solved_pos_xy(piece);
+                        let md = dist(pos, solved_pos);
+
+                        if piece == P::Piece::zero() {
+                            0
+                        } else {
+                            md
+                        }
+                    })
+                    .sum::<usize>();
+
+                if $parity_fix {
+                    // Make sure the parity is correct (some positions will give an even bound for a position
+                    // that takes an odd number of moves, etc.)
+                    let pos = puzzle.gap_position_xy();
+                    let solved_pos = puzzle.solved_pos_xy(P::Piece::zero());
+                    let parity = dist(pos, solved_pos) % 2;
+
+                    let adjusted_md = if md % 2 == parity { md } else { md + 1 };
+                    adjusted_md.as_()
                 } else {
-                    x.abs_diff(a) + y.abs_diff(b)
+                    md.as_()
                 }
-            })
-            .sum::<usize>()
-            .as_()
-    }
+            }
+        }
+    };
 }
 
-impl<T: PrimInt + Unsigned + 'static> Heuristic<Rows, T> for ManhattanDistance
-where
-    usize: AsPrimitive<T>,
-{
-    fn bound<P: SlidingPuzzle>(&self, puzzle: &P) -> T {
-        let (w, h) = puzzle.size().into();
-        let md = (0..w)
-            .cartesian_product(0..h)
-            .map(|(x, y)| {
-                let piece = puzzle.piece_at_xy((x, y));
-                let (_, b) = puzzle.solved_pos_xy(piece);
+impl_manhattan!(
+    RowGrids,
+    |(x, y), (sx, sy)| x.abs_diff(sx) + y.abs_diff(sy),
+    false,
+);
 
-                if piece == P::Piece::zero() {
-                    0
-                } else {
-                    y.abs_diff(b)
-                }
-            })
-            .sum::<usize>();
-
-        // Make sure the parity is correct (some positions will give an even bound for a position
-        // that takes an odd number of moves, etc.)
-        let (gx, gy) = puzzle.gap_position_xy();
-        let (sx, sy) = puzzle.solved_pos_xy(P::Piece::zero());
-
-        let adjusted_md = if md % 2 == (gx.abs_diff(sx) + gy.abs_diff(sy)) % 2 {
-            md
-        } else {
-            md + 1
-        };
-
-        adjusted_md.as_()
-    }
-}
+impl_manhattan!(Rows, |(_, y), (_, sy)| y.abs_diff(sy), true);
