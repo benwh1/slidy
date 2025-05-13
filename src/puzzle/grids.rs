9347f90bb25deeb47d7e38348b30200b45937ff2
Ben Whitmore <32911992+benwh1@users.noreply.github.com>
2025-05-13 09:46:31 +0100

    Fix documentation links

--- src/puzzle/grids.rs
+++ src/puzzle/grids.rs
@@ -3,6 +3,9 @@
 use crate::puzzle::{label::rect_partition::Rect, size::Size};
 
 /// Defines the grid structure of a [`Label`] or [`ColorScheme`].
+///
+/// [`Label`]: crate::puzzle::label::label::Label
+/// [`ColorScheme`]: crate::puzzle::color_scheme::ColorScheme
 pub trait Grids {
     /// Returns the [`Rect`] corresponding to a grid containing the given position on a puzzle of
     /// the given [`Size`].
