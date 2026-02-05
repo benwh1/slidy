9a8b6d0b86769f6ab0ab13dadf3bb0050425c720
Ben Whitmore <32911992+benwh1@users.noreply.github.com>
2026-02-05 00:47:17 +0000

    Add `Pdb` type aliases

--- src/solver/small/pdb.rs
+++ src/solver/small/pdb.rs
@@ -17,6 +17,31 @@ pub struct Pdb<const W: usize, const H: usize, const N: usize, MetricTag> {
     phantom_metric_tag: PhantomData<MetricTag>,
 }
 
+pub type Pdb2x2Stm = Pdb<2, 2, 4, Stm>;
+pub type Pdb2x2Mtm = Pdb<2, 2, 4, Mtm>;
+pub type Pdb2x3Stm = Pdb<2, 3, 6, Stm>;
+pub type Pdb2x3Mtm = Pdb<2, 3, 6, Mtm>;
+pub type Pdb2x4Stm = Pdb<2, 4, 8, Stm>;
+pub type Pdb2x4Mtm = Pdb<2, 4, 8, Mtm>;
+pub type Pdb2x5Stm = Pdb<2, 5, 10, Stm>;
+pub type Pdb2x5Mtm = Pdb<2, 5, 10, Mtm>;
+pub type Pdb2x6Stm = Pdb<2, 6, 12, Stm>;
+pub type Pdb2x6Mtm = Pdb<2, 6, 12, Mtm>;
+pub type Pdb3x2Stm = Pdb<3, 2, 6, Stm>;
+pub type Pdb3x2Mtm = Pdb<3, 2, 6, Mtm>;
+pub type Pdb3x3Stm = Pdb<3, 3, 9, Stm>;
+pub type Pdb3x3Mtm = Pdb<3, 3, 9, Mtm>;
+pub type Pdb3x4Stm = Pdb<3, 4, 12, Stm>;
+pub type Pdb3x4Mtm = Pdb<3, 4, 12, Mtm>;
+pub type Pdb4x2Stm = Pdb<4, 2, 8, Stm>;
+pub type Pdb4x2Mtm = Pdb<4, 2, 8, Mtm>;
+pub type Pdb4x3Stm = Pdb<4, 3, 12, Stm>;
+pub type Pdb4x3Mtm = Pdb<4, 3, 12, Mtm>;
+pub type Pdb5x2Stm = Pdb<5, 2, 10, Stm>;
+pub type Pdb5x2Mtm = Pdb<5, 2, 10, Mtm>;
+pub type Pdb6x2Stm = Pdb<6, 2, 12, Stm>;
+pub type Pdb6x2Mtm = Pdb<6, 2, 12, Mtm>;
+
 impl<const W: usize, const H: usize, const N: usize> Pdb<W, H, N, Stm>
 where
     Puzzle<W, H>: SmallPuzzle<PieceArray = [u8; N]>,
