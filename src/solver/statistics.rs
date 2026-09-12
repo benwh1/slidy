59bfe2e610d6f5107d7603fd1bc4b58fc43f002b
Ben Whitmore <32911992+benwh1@users.noreply.github.com>
2026-09-12 02:25:19 +0100

    Add serde derives on `PdbIterationStats` and `SolverIterationStats`

--- src/solver/statistics.rs
+++ src/solver/statistics.rs
@@ -1,7 +1,11 @@
 //! Defines types holding statistics related to solvers.
 
+#[cfg(feature = "serde")]
+use serde::{Deserialize, Serialize};
+
 /// Statistics about an iteration of a breadth-first search used to build a pattern database.
 #[derive(Clone, Copy, Debug, Default, PartialEq, Eq, Hash)]
+#[cfg_attr(feature = "serde", derive(Serialize, Deserialize))]
 pub struct PdbIterationStats {
     /// The greatest depth of entries currently in the PDB.
     pub depth: u8,
@@ -15,6 +19,7 @@ pub struct PdbIterationStats {
 
 /// Statistics about an iteration of a depth-first search used to solve a puzzle.
 #[derive(Clone, Copy, Debug, Default, PartialEq, Eq, Hash)]
+#[cfg_attr(feature = "serde", derive(Serialize, Deserialize))]
 pub struct SolverIterationStats {
     /// The depth of the search iteration that just finished.
     pub depth: u8,
